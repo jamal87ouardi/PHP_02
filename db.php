959b85eddf65e719397cd74819454397c7978e8d
unknown <ouardijamal87@gmail.com>
2023-04-25 12:40:36 +0000

    first

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "bismiL@hiRahman";
+$dbname = "ecommerce";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    echo("Connection failed: " . $conn->connect_error);
+}
+else {
+    echo "Connected successfully";
+}    
+ 
+?>
